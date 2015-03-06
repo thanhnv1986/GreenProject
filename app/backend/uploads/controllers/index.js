485976e3186cda9ea729936cb0e29c15031b57c2
fuguboy <thanhnv@techmaster.vn>
2015-03-06 16:29:59 +0700

    update change busboy to formiable

--- app/backend/uploads/controllers/index.js
+++ app/backend/uploads/controllers/index.js
@@ -88,47 +88,16 @@ exports.upload = function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
         folder = fields.d;
-        console.log(fields, files);
+        var folder_to_upload = standardPath + folder;
         fs.rename(files["files[]"].path, folder_to_upload + '/' + files["files[]"].name, function (err) {
             if (err) {
                 console.log(err);
-            }
-            else {
-
             }
         });
     });
     form.on('end', function() {
         res.jsonp({"res": "ok", "msg": ""});
     });
-    /*var fstream;
-     var folder;
-     req.busboy.on('field', function (key, value, keyTruncated, valueTruncated) {
-     if (key == 'd') {
-     folder = value;
-     }
-     });
-     req.busboy.on('file', function (fieldname, file, filename) {
-
-     console.log(req.param(fieldname), filename, req.body);
-     if (filename != "") {
-     var folder_to_upload = standardPath + folder;
-     fstream = fs.createWriteStream(folder_to_upload + '/' + filename);
-     file.pipe(fstream);
-     fstream.on('close', function () {
-
-     });
-     }
-     else {
-     file.resume();
-     }
-
-     });
-     req.busboy.on('finish', function () {
-     res.jsonp({"res": "ok", "msg": ""});
-     });
-     req.pipe(req.busboy);*/
-    //res.jsonp({"res":"error", "msg":"Chưa tích hợp"});
 };
 exports.download = function (req, res) {
     res.jsonp({"res": "error", "msg": "Chưa tích hợp"});
@@ -181,9 +150,8 @@ exports.thumb = function (req, res) {
     var width = req.param('width');
     var height = req.param('height');
     var tmpFolder = standardPath + '/fileman/tmp';
-    //Kiem tra xem file da ton tai chua
-    //Neu ton tai roi thi tra ve
-    //Chua ton tai thi tao file thumb
+    //Check file exit
+    //Create thumbnail
     var filename = getFileName(filePath);
     if (fs.existsSync(tmpFolder + '/' + filename)) {
         var img = fs.readFileSync(tmpFolder + '/' + filename);
